c4b32aa3fe7c73355582d2402064d3e29f7246b6
Kimberley Fraser <kimberleyfraser@Kimberleys-MacBook-Pro.local>
2018-06-18 17:29:50 -0400

    Logic added to main

--- tallybot/app/src/main/java/com/example/kimberleyfraser/tallybot/MainActivity.java
+++ tallybot/app/src/main/java/com/example/kimberleyfraser/tallybot/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.kimberleyfraser.tallybot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText word;
+    Button submit;
+    Button seeResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        word = findViewById(R.id.word);
+        submit = findViewById(R.id.submit);
+        seeResults = findViewById(R.id.results);
+
+        
     }
 }
