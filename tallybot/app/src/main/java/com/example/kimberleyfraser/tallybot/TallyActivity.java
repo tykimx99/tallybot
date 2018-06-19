cf28f53f04c73900d53b58f74c12a362b3d9d858
Yun Kim <yunkim@Yuns-MacBook-Pro.local>
2018-06-19 09:19:27 -0400

    Added navigation from main activity to tally activity.

--- tallybot/app/src/main/java/com/example/kimberleyfraser/tallybot/TallyActivity.java
+++ tallybot/app/src/main/java/com/example/kimberleyfraser/tallybot/TallyActivity.java
@@ -2,16 +2,23 @@ package com.example.kimberleyfraser.tallybot;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class TallyActivity extends ListActivity {
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
+public class TallyActivity extends AppCompatActivity {
+
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tally);
         String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
                 "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
                 "Linux", "OS/2" };
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
+//        setListAdapter(adapter);
     }
 }
